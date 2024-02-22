948bfed7fda7c7ed3b3247656380af0cc2df23b0
randyduong08 <71861208+randyduong08@users.noreply.github.com>
2024-02-21 21:08:59 -0500

    Implemented Login Page (#1)
    - Created landing page for route /login
    - Created LoginForm component for the aforementioned page
    - Created .css for LoginForm styling

---
+++ client/src/app/login/page.js
@@ -0,0 +1,12 @@
+// https://nextjs.org/learn/dashboard-app/adding-authentication
+import LoginForm from "./loginform";
+ 
+export default function LoginPage() {
+    return (
+      <main className="flex items-center justify-center md:h-screen">
+        <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+          <LoginForm/>
+        </div>
+      </main>
+    );
+  }
