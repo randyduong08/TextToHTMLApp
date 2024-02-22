76d7ab784b5a91788459e5b82c28752439f7c737
GiaviTran18 <giavitran18@gmail.com>
2024-02-21 22:54:35 -0500

    Changed Text Area
    Added textinput.css
    Added textinput.js
    Changed globals.cc
    (Changed background color to match registration)

--- client/src/app/index/ui/textinput.js
+++ client/src/app/index/ui/textinput.js
@@ -1,13 +1,12 @@
 // TextInput Component
-
-export default function Page() {
+import './textinput.css';
+export default function TextInput() {
     return (
-        <div>
-            <h2>Input Text</h2>
+        <div classname="container">
+            <h2>Enter description of website to generate:</h2>
             <textarea rows="25" cols="100">
-                Enter description of website to generate:
+            Example: Generate a video player website with a red play button.
             </textarea>
         </div>
-
     )
-}
+}   
