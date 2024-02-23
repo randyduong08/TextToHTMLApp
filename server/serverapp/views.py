df235d17a8ec3bbc2835141f349eeb42b9aef909
Riley Benjamin O'Keefe <59517733+brugbug@users.noreply.github.com>
2024-02-22 22:58:39 -0500

    Postgres DB Development (#3)
    * Add port to DB portion of docker-compose.yml
    
    * Added Models for Users and Prompts tables and a View for handling Prompt submissions
    
    Also added some notes on the Django serverapp project files
    
    * Change TextInput Component into a React Form for Submission

--- server/serverapp/views.py
+++ server/serverapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Prompt
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
+# NOTE: Views in Django are "Request Handlers", not to be confused with front-end views
+# NOTE: View functions are functions that take a request and return a response; "request handlers"
+
+# This view / request handler will handle storing the user's prompt in the database
+@csrf_exempt # disables CSRF protection, maybe get rid of this when we push to production
+def store_prompt(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        prompt = Prompt(user_id=data['userID'], prompt=data['prompt'])
+        prompt.save()
+        return JsonResponse({"success": "Prompt saved successfully"})
+    return JsonResponse({"error": "Method not allowed"}, status=405)
