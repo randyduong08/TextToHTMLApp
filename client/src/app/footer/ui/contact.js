ab3b6cd6a8bd01bdbba02dca158c794f8e4a66e6
bluelinkthe8 <94149131+bluelinkthe8@users.noreply.github.com>
2024-02-25 19:17:57 -0500

    Header footer development (#5)
    * Create initial Navigation Bar
    
    Created initial navigation bar layout, including:
    Home Button
    Login Button
    Register Button
    
    * Implemented Navigation Links
    
    Changed buttons in navigation bar into links which redirect to their respective pages
    
    * Header Fixes
    
    Header is now displays properly on all pages (Home, Register, and Login)
    
    * Implemented Footer Prototype
    
    Minimal Footer structure implemented
    
    * Footer Creation
    
    Implemented Footer, List of Members, and GitHub Link (proper link to-do)
    
    * Header stylized
    
    Header stylized to look more sleek
    
    * Footer fix and style implemented
    
    Fixed padding and height of footer
    Added style for footer
    
    * Adjusted index page syling
    
    Removed margin of body in global.css
    Removed borders on footer and header
    Adjusted colors of index page to be similar to color scheme of login and register pages
    
    * Updated github logo
    
    Inverted colors for github logo
    
    ---------
    
    Co-authored-by: randyduong08 <randyduong08@gmail.com>

---
+++ client/src/app/footer/ui/contact.js
@@ -0,0 +1,13 @@
+export default function Page(){
+    return(
+        <div className='contact-info'>
+            <h3>Members</h3>
+            <ul>
+                <li>Randy Duong</li>
+                <li>Riley O'Keefe</li>
+                <li>Giavi Tran</li>
+                <li>Jonathan Truong</li>
+            </ul>
+        </div>
+    )
+}
