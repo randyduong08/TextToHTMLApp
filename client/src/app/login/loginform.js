3b4fe831e170397eef17d44e5a5d8cab6daab086
GiaviTran18 <123785404+GiaviTran18@users.noreply.github.com>
2024-02-25 19:38:30 -0500

    Pull Request Test 1 (#6)
    * Changed Text Area
    
    Added textinput.css
    Added textinput.js
    Changed globals.cc
    (Changed background color to match registration)
    
    * Conformal Stylizing of Login and Registration
    
    Changed Login.css
    Changed Color and Background to match Textinput
    Changed Registration.css
    Changed Color and Background to match Textinput
    
    * Create initial Navigation Bar
    
    Created initial navigation bar layout, including:
    Home Button
    Login Button
    Register Button
    
    * Added images and stylized login/register
    
    Changed registration form css and js
    added images to buttons and set max width
    Changed login form css and js
    added images to buttons and set max width
    
    Changed both to light blue colour to match textinput
    
    * Changed font and coloring
    
    Changed font and coloring of registration and login css
    
    * Implemented Navigation Links
    
    Changed buttons in navigation bar into links which redirect to their respective pages
    
    * Header Fixes
    
    Header is now displays properly on all pages (Home, Register, and Login)
    
    * Implemented Footer Prototype
    
    Minimal Footer structure implemented
    
    * Submit Button Styling
    
    Edited submitbutton js and created submitbutton css
    
    NOTE not currently working
    
    * File Input JS and css
    
    Created file input JS and CSS
    Stylized the input button for submitting text files exclusively
    
    ---------
    
    Co-authored-by: bluelinkthe8 <Theragingkirbz@gmail.com>
    Co-authored-by: randyduong08 <randyduong08@gmail.com>

--- client/src/app/login/loginform.js
+++ client/src/app/login/loginform.js
@@ -4,8 +4,12 @@ export default function LoginForm(){
     return(
         <div className="container">
             <h2 className="flex items-center justify-center">Login to TextToHTML</h2>
-            <button className="button google">Log in with Google</button>
-            <button className="button microsoft">Log in Microsoft</button>
+            <button className="button google">
+            <img src="Google Login Icon.png" alt="Google Login Icon" width="5%" height="5%"/>Log in with Google
+            </button>
+            <button className="button microsoft">
+            <img src="Windows Login Icon.png" alt="Windows Login Icon" width="5%" height="5%"/>Log in with Microsoft
+            </button>
             <p className="flex items-center justify-center">or</p>
             <form>
                 <input type="email" placeholder="Email" className="email" />
--- client/src/app/registration/registrationform.js
+++ client/src/app/registration/registrationform.js
@@ -4,8 +4,12 @@ export default function RegistrationForm(){
     return(
         <div className="container">
             <h2 className="flex items-center justify-center">Register for TextToHTML</h2>
-            <button className="button google">Register using Google</button>
-            <button className="button microsoft">Register using Microsoft</button>
+            <button className="button google">
+            <img src="Google Login Icon.png" alt="Google Login Icon" width="5%" height="5%" style={{ marginRight: '5px' }} />Register with Google
+            </button>
+            <button className="button microsoft">
+            <img src="Windows Login Icon.png" alt="Windows Login Icon" width="5%" height="5%"/>Register with Microsoft
+            </button>
             <p className="flex items-center justify-center">or</p>
             <form>
                 <input type="email" placeholder="Email" className="email" />
