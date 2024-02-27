d9f58574b2150c208ccf53baa4c4505b9cbb9846
okeefer <okeefer@uwindsor.ca>
2024-02-27 18:03:35 -0500

    Create serializers.py
    Created serializers to convert the JSON data from Client-side to Python native objects

---
+++ server/serverapp/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from serverapp.models import Prompt
+
+# serializers to convert JSON data to Python native objects
+
+# for User data
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['userID', 'userName', 'userEmail', 'userPassword', 'registrationDate']
+        extra_kwargs = {'userPassword': {'write_only': True}}  # It's better to handle passwords securely
+
+# for Prompt data
+class PromptSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Prompt
+        fields = ['promptID', 'userID', 'prompt', 'creationTime']
