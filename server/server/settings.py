b001caacc23ce9a45e1edf78891fbd1384c7a7b9
okeefer <okeefer@uwindsor.ca>
2024-02-28 01:23:39 -0500

    Update settings.py
    Add corsheaders to specify that we want Django to accept requests from localhost:3000 (the client frontend)

--- server/server/settings.py
+++ server/server/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # third-party apps
-    "rest_framework",
+    'corsheaders',
+    'rest_framework',
 
     # our backend app
     'serverapp',
@@ -48,11 +49,18 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'server.urls'
