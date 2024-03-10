4f94964f4f9a81be601e55ae7f96b7e640d32e7c
GiaviTran18 <giavitran18@gmail.com>
2024-03-10 18:50:42 -0400

    Adjusted footer, changed contacts.

--- client/src/app/footer/ui/contact.js
+++ client/src/app/footer/ui/contact.js
@@ -1,13 +1,13 @@
-export default function Page(){
-    return(
-        <div className='contact-info'>
-            <h3>Members</h3>
-            <ul>
-                <li>Randy Duong</li>
-                <li>Riley O'Keefe</li>
-                <li>Giavi Tran</li>
-                <li>Jonathan Truong</li>
-            </ul>
+export default function Page() {
+    return (
+        <div className="mr-40 pr-40 absolute inset-y-0 left-0 "> {/* Apply margin to the outside of the card */}
+            <div className="bg-transparent border border-white shadow-md rounded-lg p-6 text-white">
+                <h3 className="text-lg font-semibold mb-4">Members</h3>
+                    <li>Randy Duong</li>
+                    <li>Riley O'Keefe</li>
+                    <li>Giavi Tran</li>
+                    <li>Jonathan Truong</li>
+            </div>
         </div>
-    )
+    );
 }
