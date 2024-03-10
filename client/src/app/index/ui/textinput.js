ff8b501aa6dcde426a98a5a8a94903a7d2637004
GiaviTran18 <123785404+GiaviTran18@users.noreply.github.com>
2024-03-10 18:51:43 -0400

    Stylizing components (#15)
    * Fixed size of things on text input.
    
    * Adjusted footer, changed contacts.

--- client/src/app/index/ui/textinput.js
+++ client/src/app/index/ui/textinput.js
@@ -4,58 +4,41 @@
 'use client'    // React only works on client components, so need to specify this is a client component
 
 import { useState } from 'react';
-import { toast } from 'react-toastify'
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
-import { Terminal } from "lucide-react"
 
 export default function InputForm() {
-    // promptDetails = state variable; setPromptDetails = setter function, '' = initial value of prompt
-    const [promptDetails, setPromptDetails] = useState('');
+    // prompt = state variable; setPrompt = setter function, '' = initial value of prompt
+    const [prompt, setPrompt] = useState('');
 
-    // define toast messages
-    const showToastStoreSuccess = () => toast('Prompt details stored successfully');
-    const showToastStoreFail = () => toast('Error storing prompt details');
-    const showToastError = (error) => toast('Error:', error);
-
-    // function that handles submission from the html form defined below
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://localhost:8000/prompts/', {
+            const response = await fetch('http://localhost:8000/prompt_submit/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ promptDetails, userID: 0 }), // set userID to 0 TEMPORARILY
+                body: JSON.stringify({ userID: '0', prompt: prompt }), // set userID to 0 TEMPORARILY
             });
             if (response.ok) {
-                console.log('Prompt details stored successfully');
-                showToastStoreSuccess();
+                console.log('Prompt stored successfully');
             } else {
-                console.log('Error storing prompt details');
-                showToastStoreFail();
+                console.log('Error storing prompt');
             }
-
+            const data = await response.json();
+            console.log('Prompt saved:', data);
         } catch (error) {
             console.error('Error:', error);
-            showToastError(error);
         }
     };
 
     return (
         <div>
-            <Alert>
-  <Terminal className="h-4 w-4" />
-  <AlertTitle>Heads up!</AlertTitle>
-  <AlertDescription>
-    You can add components and dependencies to your app using the cli.
-  </AlertDescription>
-</Alert>
-            <h2>Input Text</h2>
+            <h2>Enter a prompt</h2>
             <form onSubmit={handleSubmit}>
                 <textarea 
-                    value={promptDetails}
-                    onChange={(e) => setPromptDetails(e.target.value)}
+                    class="resize w-96 h-96"
+                    value={prompt}
+                    onChange={(e) => setPrompt(e.target.value)}
                     placeholder="Enter description of website to generate:"
                 />
                 <br></br>
