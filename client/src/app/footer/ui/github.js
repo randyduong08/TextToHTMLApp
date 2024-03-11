4a6dc309141160b4b21f8afd0b02d06856738ef2
GiaviTran18 <giavitran18@gmail.com>
2024-03-10 20:07:01 -0400

    Adjusting sizes of a few things
    Trying to make it fit nicely on the page

--- client/src/app/footer/ui/github.js
+++ client/src/app/footer/ui/github.js
@@ -1,6 +1,6 @@
 export default function Page() {
     return (
-        <div className="mr-40 pr-40 absolute bottom-0 right-40"> {/* Apply margin to the outside of the card */}
+        <div className="mr-40 pr-40 absolute bottom-10 right-40"> {/* Apply margin to the outside of the card */}
             <div className="bg-transparent border border-white shadow-md rounded-lg p-6 text-white">
                 <h3 className="text-lg font-semibold mb-4">GitHub</h3>
                 <div className="flex items-center gap-4">
--- client/src/app/index/ui/fileinput.js
+++ client/src/app/index/ui/fileinput.js
@@ -1,12 +1,13 @@
 // FileInput.js
 
 import './fileinput.css';
-import React from 'react'
-import { useDrag } from 'react-dnd'
+import React from 'react';
+import { useDrag } from 'react-dnd';
+
 export default function Page() {
   return (
     <div className="container">
-      <h1>Submit a text file</h1>
+      <h1 className="mt-4">Submit a text file</h1> {/* Added mt-4 class for margin-top */}
       {/* Apply the file input styles to this input element */}
       <input type="file" id="myfile" name="myfile" accept="text/plain" />
     </div>
