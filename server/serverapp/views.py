1e209d2552150f6ad004886ae32aa007d7cf6c39
randyduong08 <71861208+randyduong08@users.noreply.github.com>
2024-03-17 19:18:28 -0400

    generate genre and sentiment for user input (#24)
    - successfully generate genre and sentiment for user prompts
    - converted sentiment_model and genre_model to singleton pattern

--- server/serverapp/views.py
+++ server/serverapp/views.py
@@ -7,6 +7,7 @@ from serverapp.models import User, Prompt
 from serverapp.serializers import UserSerializer, PromptSerializer
 from serverapp.data_processing import delimit_prompt_details
 from text_classification.genre_model import GenreModel
+from sentiment_analysis.sentiment_model import SentimentModel
 
 # Create your views here.
 # NOTE: Views in Django are "Request Handlers", not to be confused with front-end views
@@ -21,8 +22,11 @@ class PromptView(APIView):
     def post(self, request):
         serializer = PromptSerializer(data=request.data)
         if serializer.is_valid():
-            genre_sentiment: str = GenreModel().predict(str(serializer.data))
             serializer.save()
+            sentiment_label: str = SentimentModel().predict(str(serializer.data))
+            genre_class: str = GenreModel().predict(str(serializer.data))
+            print(genre_class)
+            print(sentiment_label)
             tokens_array = delimit_prompt_details(serializer.data)              # parse the tokens from the prompt details
             return Response(data=tokens_array, status=status.HTTP_201_CREATED)  # return these tokens
             #return Response(serializer.data, status=status.HTTP_201_CREATED)
