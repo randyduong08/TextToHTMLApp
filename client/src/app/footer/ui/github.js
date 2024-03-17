491a57bcc1f32f88a58c907196e11d447285d273
GiaviTran18 <giavitran18@gmail.com>
2024-03-17 17:55:20 -0400

    adjusted various margins and sizings.

--- client/src/app/footer/ui/github.js
+++ client/src/app/footer/ui/github.js
@@ -1,9 +1,9 @@
 export default function Page(){
     return(
-        <div className='github-info absolute inset-y-0 right-10'> {/* Apply margin to the outside of the card */}
-            <div className="bg-transparent border border-white shadow-md rounded-lg p-6 text-white">
-                <h3 className="mb-10">GitHub</h3> {/* Add margin to the bottom */}
-                <a href="https://github.com/" target="_blank"><img src="/github_logo.svg"></img></a>
+        <div className='github-info absolute inset-y-0 bottom-0 '>
+            <div className="bg-transparent border border-white shadow-md rounded-lg p-6 text-white" style={{ width: '200px', height: '190px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                <h3 className="mb-2">GitHub</h3>
+                <a href="https://github.com/" target="_blank"><img src="/github_logo.svg" alt="GitHub Logo" className="w-10 h-10"></img></a>
             </div>
         </div>
     )
--- client/src/app/index/ui/fileoutput.js
+++ client/src/app/index/ui/fileoutput.js
@@ -3,7 +3,7 @@
 export default function Page() {
     return (
         <div>
-            <h2>Download Generated HTML File</h2>
+            <h2 className="mt-4 font-bold">Download Generated HTML File</h2>
             <a href="/sampleoutput.html" download="sampleoutput.html">
             <button type="submit" className="submitButton">Download Output</button>
             </a>
