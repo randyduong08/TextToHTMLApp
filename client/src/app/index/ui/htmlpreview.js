f3787ac7914be2a82d75a29c48a9d9814292def8
Riley Benjamin O'Keefe <59517733+brugbug@users.noreply.github.com>
2024-03-18 00:02:06 -0400

    View generated webpages (#27)
    * Add Context to share data between components
    
    This is for updating htmlpreview and fileoutput when textinput or fileinput gets a successful response from server
    
    * Make a label for the download button with the filename

--- client/src/app/index/ui/htmlpreview.js
+++ client/src/app/index/ui/htmlpreview.js
@@ -1,11 +1,26 @@
 // HTMLPreview Component
 
+'use client'    // React only works on client components, so need to specify this is a client component
+
+import { useState, useEffect } from 'react';
+import { useHtmlContent } from '../../context/HtmlContentContext';
+
 export default function Page() {
+    const { htmlContent } = useHtmlContent();
+    const [iframeSrc, setIframeSrc] = useState('/sampleoutput.html');
+
+    useEffect(() => {
+        if (htmlContent) {
+            const htmlSrc = `data:text/html;charset=utf-8,${encodeURIComponent(htmlContent)}`;
+            setIframeSrc(htmlSrc);
+        }
+    }, [htmlContent]);
+
     return (
         <div>
             <h2 className="mt-4 font-bold text-2xl">Generated Web Page Preview</h2>
             {/* <embed type="text/html" src="/sampleoutput.html" width="90%" height="380"></embed> */}
-            <iframe src="/sampleoutput.html" width="90%" height="380"></iframe>
+            <iframe src={iframeSrc} width="90%" height="380"></iframe>
         </div>
     )
 }
--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,26 +1,29 @@
 import { Inter } from 'next/font/google'
 import Header from './header/headerlayout'
 import Footer from './footer/footerlayout'
-import {SessionProvider} from "next-auth/react";
+import { SessionProvider } from "next-auth/react";
 import './globals.css'
+import { HtmlContentProvider } from './context/HtmlContentContext';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Text to HTML App',
-  description: 'Generate HTML code from user input!',
+    title: 'Text to HTML App',
+    description: 'Generate HTML code from user input!',
 }
 
 export default function RootLayout({session, children }) {
-  return (
-    <SessionProvider session={session}>
-      <html lang="en">
-        <body className={inter.className}>
-          <Header />
-          {children}
-          <Footer />
-        </body>
-      </html>
-    </SessionProvider>
-  )
+    return (
+        <HtmlContentProvider>
+            <SessionProvider session={session}>
+                <html lang="en">
+                    <body className={inter.className}>
+                        <Header />
+                            {children}
+                        <Footer />
+                    </body>
+                </html>
+            </SessionProvider>
+        </HtmlContentProvider>
+    )
 }
