009ce138a912c8e826ebc769d87e89d92c1c8326
okeefer <okeefer@uwindsor.ca>
2024-03-20 16:38:17 -0400

    Resize the preview
    Move previewpage button to htmlpreview.js

--- client/src/app/index/ui/fileoutput.js
+++ client/src/app/index/ui/fileoutput.js
@@ -18,11 +18,6 @@ export default function Page() {
 
     return (
         <div className="mt-2 flex-auto">
-            <div className="mt-1 mb-2 flex-auto">
-                <a href="http://localhost:8000/generated-page/">
-                    <button type="submit" className="submitButton" style={{ width: '172px' }}>Preview Page</button>
-                </a>
-            </div>
             <h2 className="font-bold text-2xl">Download Generated HTML File</h2>
             <a href={downloadSrc} download="generated_page.html">
                 <button type="submit" className="submitButton">Download Output</button>
