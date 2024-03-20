0ae11dc0cb19043603828198e19f4d173c1379c5
okeefer <okeefer@uwindsor.ca>
2024-03-20 16:58:06 -0400

    Fixed some styling

--- client/src/app/index/ui/fileoutput.js
+++ client/src/app/index/ui/fileoutput.js
@@ -18,7 +18,9 @@ export default function Page() {
 
     return (
         <div className="mt-2 flex-auto">
-            <h2 className="font-bold text-2xl">Download Generated HTML File</h2>
+            <h2 className="font-bold text-2xl mt-8 mb-2">Download Generated HTML File</h2>
+            <p>Click below to download the currently generated .html webpage!</p>
+            <p className="mb-2">(Shown above)</p>
             <a href={downloadSrc} download="generated_page.html">
                 <button type="submit" className="submitButton">Download Output</button>
                 <label class="ml-2">generated_page.html</label>
